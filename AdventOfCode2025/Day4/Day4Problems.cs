00a1e6d45f0d65cba8094e48d819b90e99213d6a
Tom Warner <tomwarner13@gmail.com>
2025-12-04 09:31:48 -0500

    d4p2

--- AdventOfCode2025/Day4/Day4Problems.cs
+++ AdventOfCode2025/Day4/Day4Problems.cs
@@ -59,6 +59,80 @@ public class Day4Problems : Problems
 
   protected override string Problem2(string[] input, bool isTestInput)
   {
-    throw new NotImplementedException();
+    //first pass through: copypasta above. build 2 hashsets: every roll, and removable rolls
+    //then while removableRolls.Any():
+    //  everyRoll -= removables
+    //  removables.clear
+    //  loop through new everyRoll and check for removables, add to set if so
+    //return everyRoll.count
+    
+    var bounds = new GridPoint(input[0].Length, input.Length);
+    var removableRolls = new HashSet<GridPoint>();
+    var allRolls = new HashSet<GridPoint>();
+    
+    for (var y = 0; y < input.Length; y++)
+    {
+      for (var x = 0; x < input[0].Length; x++)
+      {
+        var checkChar = input[y][x];
+        switch (checkChar)
+        {
+          case '.':
+            //no-op
+            break;
+          default:
+            var curPoint = new GridPoint(x, y);
+            allRolls.Add(curPoint);
+            var neighboringRolls = 0;
+            foreach (var dir in GridPoint.ExtendedDirections)
+            {
+              var neighbor = curPoint + dir;
+              if (neighbor.IsInBounds(bounds) &&
+                  input[neighbor.Y][neighbor.X] == '@')
+              {
+                neighboringRolls++;
+              }
+            }
+
+            if (neighboringRolls < 4)
+            {
+              D(curPoint);
+              removableRolls.Add(curPoint);
+            }
+            break;
+        }
+      }
+    }
+
+    var originalTotal = allRolls.Count;
+    D($"removable: {removableRolls.Count} | all: {originalTotal}");
+    while (removableRolls.Count > 0)
+    {
+      //remove rolls
+      allRolls.ExceptWith(removableRolls);
+      removableRolls.Clear();
+
+      foreach (var roll in allRolls)
+      {
+        var neighboringRolls = 0;
+        foreach (var dir in GridPoint.ExtendedDirections)
+        {
+          var neighbor = roll + dir;
+          if (allRolls.Contains(neighbor))
+          {
+            neighboringRolls++;
+          }
+        }
+
+        if (neighboringRolls < 4)
+        {
+          removableRolls.Add(roll);
+        }
+      }
+      
+      D($"removable: {removableRolls.Count} | all: {allRolls.Count}");
+    }
+    
+    return (originalTotal - allRolls.Count).ToString();
   }
 }
