be0886fe9ffad4b2963a89f020081030f15813b6
Tom Warner <tomwarner13@gmail.com>
2025-12-17 14:02:11 -0500

    day 12 part 1: write stupid problems get stupid solutions

--- AdventOfCode2025/Day12/Day12Problems.cs
+++ AdventOfCode2025/Day12/Day12Problems.cs
@@ -6,11 +6,26 @@ public class Day12Problems : Problems
 {
   protected override int Day => 12;
   
-  protected override string TestInput => @"";
+  protected override string TestInput => @"can we get a new guy to make AoC next year?";
 
   public override string Problem1(string[] input, bool isTestInput)
   {
-    throw new NotImplementedException();
+    var regionsFitting = 0;
+
+    foreach (var line in input)
+    {
+      var ints = StringUtils.ExtractIntsFromString(line).ToArray();
+      
+      if(ints.Length < 3) continue;
+      
+      var area = ints[0] * ints[1];
+      
+      var totalShapesRequired = ints[2..].Sum();
+      
+      regionsFitting += area >= (totalShapesRequired * 8) ? 1 : 0;
+    }
+    
+    return regionsFitting.ToString();
   }
 
   public override string Problem2(string[] input, bool isTestInput)
